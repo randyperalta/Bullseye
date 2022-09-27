38913b657b9bb3a0de83b7bc2d828b7396ff8956
Randy Peralta <randyperalta@outlook.com>
2022-09-27 19:04:10 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Randy Peralta on 27/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
